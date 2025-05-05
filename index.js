1ad1918adaa2646eb9333ca3d1949b293362eebc
alpha-code-labs <138210698+alpha-code-labs@users.noreply.github.com>
2025-05-05 18:48:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,14 +64,6 @@ console.log("I finished express and json")
 
 
 // Add request logging middleware
-console.log("I am about to log middleware")
-app.use((req, res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-  next();
-});
-console.log("I finished log middleware")
-
-
 // Enable CORS preflight for all routes
 console.log("I am about to options cors")
 app.options('*', cors());
@@ -150,6 +142,13 @@ app.use('/api', readingRoutes);
 app.use('/api', listeningRoutes);
 app.use('/api', subscriptionRoutes);
 console.log("I finished register routes")
+
+console.log("I am about to log middleware")
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+console.log("I finished log middleware")
 
 
 // Error handling middleware
