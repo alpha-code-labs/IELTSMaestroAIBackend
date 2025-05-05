77c20eadd00fc7bd42323177dbfc2df94107b4d1
alpha-code-labs <138210698+alpha-code-labs@users.noreply.github.com>
2025-05-05 20:25:48 +0530

    Update session.js

--- models/session.js
+++ models/session.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-
 // Schema for session counter
 const sessionSchema = new mongoose.Schema({
-  counter: { type: Number, default: 0 }
+  counter: { type: Number, default: 0 },
+  trackedSessions: { type: [String], default: [] }
 });
-
 // Create model
 const SessionCounter = mongoose.model('IELTSUsers', sessionSchema);
-
 module.exports = SessionCounter;
